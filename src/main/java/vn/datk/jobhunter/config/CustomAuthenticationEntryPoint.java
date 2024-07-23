43f164f87ceb8ef0d8f2369082c7c9c2f8cfdddf
TranDatk <hgooshvhd123@gmail.com>
2024-07-23 22:11:58 +0700

    update

--- src/main/java/vn/datk/jobhunter/config/CustomAuthenticationEntryPoint.java
+++ src/main/java/vn/datk/jobhunter/config/CustomAuthenticationEntryPoint.java
@@ -14,6 +14,7 @@ import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
@@ -31,7 +32,12 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
 
         RestResponse<Object> res = new RestResponse<Object>();
         res.setStatusCode(HttpStatus.UNAUTHORIZED.value());
-        res.setError(authException.getCause().getMessage());
+
+        String errorMessage = Optional.ofNullable(authException.getCause())
+                .map(Throwable::getMessage)
+                .orElse(authException.getMessage());
+
+        res.setError(errorMessage);
         res.setMessage("Token is invalid (expired, not in the correct format, or does not transmit JWT in the header)...");
 
         mapper.writeValue(response.getWriter(), res);
--- src/main/java/vn/datk/jobhunter/config/SecurityConfiguration.java
+++ src/main/java/vn/datk/jobhunter/config/SecurityConfiguration.java
@@ -49,10 +49,10 @@ public class SecurityConfiguration {
                         (oauth2) -> oauth2.jwt(Customizer.withDefaults())
                                 .authenticationEntryPoint(caep)
                 )
-                .exceptionHandling(
-                        exceptions -> exceptions
-                                .authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint()) //401
-                                .accessDeniedHandler(new BearerTokenAccessDeniedHandler())) //403
+//                .exceptionHandling(
+//                        exceptions -> exceptions
+//                                .authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint()) //401
+//                                .accessDeniedHandler(new BearerTokenAccessDeniedHandler())) //403
                 .formLogin(f -> f.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
