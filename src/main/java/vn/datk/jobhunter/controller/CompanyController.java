43a80751f30f09948d1dd0ffd618b7ce1fdbf6c7
TranDatk <hgooshvhd123@gmail.com>
2024-07-25 22:37:35 +0700

    feat(CompanyController): Crud for Company

--- src/main/java/vn/datk/jobhunter/controller/CompanyController.java
+++ src/main/java/vn/datk/jobhunter/controller/CompanyController.java
@@ -5,12 +5,11 @@ import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import vn.datk.jobhunter.domain.Company;
 import vn.datk.jobhunter.service.CompanyService;
+
+import java.util.List;
 
 
 @RequestMapping(path = "${apiPrefix}/companies")
@@ -24,5 +23,27 @@ public class CompanyController {
     public ResponseEntity<Company> createCompany(@Valid @RequestBody Company company){
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(this.companyService.createCompany(company));
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<Company>> getCompany(){
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(this.companyService.getCompany());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Company> updateCompany(
+            @PathVariable("id") Long id,
+            @Valid @RequestBody Company company
+    ){
+        return ResponseEntity.ok(this.companyService.updateCompany(id, company));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> deleteCompany(
+            @PathVariable("id") Long id
+    ){
+        this.companyService.deleteCompany(id);
+        return ResponseEntity.ok(null);
     }
 }
--- src/main/java/vn/datk/jobhunter/service/CompanyService.java
+++ src/main/java/vn/datk/jobhunter/service/CompanyService.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Service;
 import vn.datk.jobhunter.domain.Company;
 import vn.datk.jobhunter.repository.CompanyRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 @RequiredArgsConstructor
 @Service
 public class CompanyService {
@@ -12,5 +15,26 @@ public class CompanyService {
 
     public Company createCompany(Company company){
         return companyRepository.save(company);
+    }
+
+    public List<Company> getCompany(){
+        return companyRepository.findAll();
+    }
+
+    public Company updateCompany(Long id, Company company){
+        Optional<Company> companyOptional = this.companyRepository.findById(id);
+        if(companyOptional.isPresent()){
+            Company currentCompany = companyOptional.get();
+            currentCompany.setName(company.getName());
+            currentCompany.setLogo(company.getLogo());
+            currentCompany.setDescription(company.getDescription());
+            currentCompany.setAddress(company.getAddress());
+            return this.companyRepository.save(currentCompany);
+        }
+        return null;
+    }
+
+    public void deleteCompany(Long id){
+        this.companyRepository.deleteById(id);
     }
 }
